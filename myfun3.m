e2be07097a9ab0cc428ede6a2b050dba44da4c84
LongyanU <229440816@qq.com>
2019-08-07 15:54:08 +0800

    Add files via upload

--- myfun3.m
+++ myfun3.m
@@ -1,17 +1,12 @@
-function F = myfun2(x)
+function F = myfun3(x)
 
-global M  h ratio
+global M  ratio h
 k=linspace(1/1000000,ratio*pi/h,100);
-
 temp1=0;
 for m=1:M
     temp1=temp1+2*x(m)*sin((m-1/2)*k*h);
 end
 
-temp=2*temp1.*sin(0.5*k*h);
-
-
-
-
+temp=temp1.^2;
 
 F=(temp-k.^2*h^2)*(temp-k.^2*h^2)';
